6d4b2ee5584af3706f14200273136920e5d3f8b5
Calvin So <calvinchso@gmail.com>
2021-05-11 09:16:08 -0700

    Added private data collection capability

--- app/controllers/asset.controller.js
+++ app/controllers/asset.controller.js
@@ -24,6 +24,205 @@ exports.enroll = async (req, res) => {
 }
 
 //
+// GET /api/asset/initPrivateData
+//
+exports.initPrivateData = async (req, res) => {
+
+    let gateway;            
+
+    try {
+        gateway = await BlockchainHelper.getGateway();
+        let network = await BlockchainHelper.getNetwork(gateway, Constants.CHANNEL_NAME);
+        let contract = BlockchainHelper.getContract(network, Constants.CHAINCODE_NAME);
+        // Initialize a set of asset data on the channel using the chaincode 'InitLedger' function.
+        // This type of transaction would only be run once by an application the first time it was started after it
+        // deployed the first time. Any updates to the chaincode deployed later would likely not need to run
+        // an "init" type function.
+        console.log('--> Submit Transaction: InitPrivateData, function creates the initial set of assets on the private data collection');
+        await contract.submitTransaction('InitPrivateData');
+        console.log('*** Result: committed');
+        res.send({message: 'InitPrivateData ran successfully'})
+    } catch (error) {
+        console.error(`InitPrivateData failed to run: ${error.message}`);
+        console.error( error.stack );
+        res.send({message: 'InitPrivateData failed to run'})
+    } finally {
+        console.log('Disconnect from Fabric gateway.');    
+        if (gateway != null) { gateway.disconnect(); }
+    }
+
+}
+
+//
+// GET /api/asset/getAllPrivateData
+//
+exports.getAllPrivateData = async (req, res) => {
+
+    let gateway;            
+
+    try {
+        gateway = await BlockchainHelper.getGateway();
+        let network = await BlockchainHelper.getNetwork(gateway, Constants.CHANNEL_NAME);
+        let contract = BlockchainHelper.getContract(network, Constants.CHAINCODE_NAME);
+        console.log('--> Submit Transaction: GetAllPrivateData, function returns all the current assets on the ledger');
+        let result = await contract.submitTransaction('GetAllPrivateData');
+        res.send({getAllPrivateData: StringHelper.toJSONObject(result)})
+    } catch (error) {
+        console.error(`GetAllPrivateData failed to run: ${error.message}`);
+        console.error( error.stack );
+        res.send({message: 'GetAllPrivateData failed to run'})
+    } finally {
+        console.log('Disconnect from Fabric gateway.');    
+        if (gateway != null) { gateway.disconnect(); }
+    }
+
+}
+
+//
+// GET /api/asset/createPrivateData
+//
+exports.createPrivateData = async (req, res) => {
+
+    let gateway;            
+
+    try {
+        gateway = await BlockchainHelper.getGateway();
+        let network = await BlockchainHelper.getNetwork(gateway, Constants.CHANNEL_NAME);
+        let contract = BlockchainHelper.getContract(network, Constants.CHAINCODE_NAME);
+        console.log('--> Submit Transaction: CreatePrivateData, creates new asset with ID, color, owner, size, and appraisedValue arguments');
+
+        const asset = {
+            ID:  Buffer.from(`asset${faker.datatype.number()}`),
+            Color: Buffer.from(faker.commerce.color()),
+            Size: Buffer.from(faker.datatype.number().toString()),
+            Owner: Buffer.from(faker.name.firstName()),
+            AppraisedValue: Buffer.from(faker.datatype.number().toString())
+        }
+
+        const result = await contract.createTransaction('CreatePrivateData')
+                        .setTransient(asset)
+                        .submit();        
+
+        res.send({createPrivateData: StringHelper.toJSONObject(result)})
+    } catch (error) {
+        console.error(`createPrivateData failed to run: ${error.message}`);
+        console.error( error.stack );
+        res.send({message: 'createPrivateData failed to run'})
+    } finally {
+        console.log('Disconnect from Fabric gateway.');    
+        if (gateway != null) { gateway.disconnect(); }
+    }
+
+}
+
+//
+// GET /api/asset/readPrivateData
+//
+exports.readPrivateData = async (req, res) => {
+
+    let gateway;            
+    let assetId = req.query.assetId || 'asset103'
+
+    try {
+        gateway = await BlockchainHelper.getGateway();
+        let network = await BlockchainHelper.getNetwork(gateway, Constants.CHANNEL_NAME);
+        let contract = BlockchainHelper.getContract(network, Constants.CHAINCODE_NAME);
+        console.log('\n--> Submit Transaction: ReadPrivateData, function returns an asset with a given assetID');
+        result = await contract.submitTransaction('ReadPrivateData', assetId);
+        res.send({readPrivateData: StringHelper.toJSONObject(result)})
+    } catch (error) {
+        console.error(`readPrivateData failed to run: ${error.message}`);
+        console.error( error.stack );
+        res.send({message: 'readPrivateData failed to run'})
+    } finally {
+        console.log('Disconnect from Fabric gateway.');    
+        if (gateway != null) { gateway.disconnect(); }
+    }
+
+}
+
+//
+// GET /api/asset/updatePrivateData
+//
+exports.updatePrivateData = async (req, res) => {
+
+    let gateway;            
+    let assetId = req.query.assetId || 'asset103'
+
+    try {
+        gateway = await BlockchainHelper.getGateway();
+        let network = await BlockchainHelper.getNetwork(gateway, Constants.CHANNEL_NAME);
+        let contract = BlockchainHelper.getContract(network, Constants.CHAINCODE_NAME);
+
+        const asset = {
+            ID:  Buffer.from(assetId),
+            Color: Buffer.from(faker.commerce.color()),
+            Size: Buffer.from(faker.datatype.number().toString()),
+            Owner: Buffer.from(faker.name.firstName()),
+            AppraisedValue: Buffer.from(faker.datatype.number().toString())
+        }
+
+        console.log(`--> Submit Transaction: UpdatePrivateData ${assetId}, change all of its values`);
+        await contract.createTransaction('UpdatePrivateData')
+                .setTransient(asset)
+                .submit();        
+
+        console.log('--> Submit Transaction: ReadPrivateData, function returns an asset with a given assetID');
+        result = await contract.submitTransaction('ReadPrivateData', assetId);
+    
+        res.send({status: 'OK', afterUpdatePrivateDataAssetValues: StringHelper.toJSONObject(result)})
+    } catch (error) {
+        console.error(`updatePrivateData failed to run: ${error.message}`);
+        console.error( error.stack );
+        res.send({message: 'updatePrivateData failed to run'})
+    } finally {
+        console.log('Disconnect from Fabric gateway.');    
+        if (gateway != null) { gateway.disconnect(); }
+    }
+
+}
+
+//
+// GET /api/asset/transferPrivateData
+//
+exports.transferPrivateData = async (req, res) => {
+
+    let gateway;            
+    let assetId = req.query.assetId || 'asset103'
+    let newOwner = faker.name.firstName()
+
+    try {
+        gateway = await BlockchainHelper.getGateway();
+        let network = await BlockchainHelper.getNetwork(gateway, Constants.CHANNEL_NAME);
+        let contract = BlockchainHelper.getContract(network, Constants.CHAINCODE_NAME);
+
+        const asset = {
+            ID:  Buffer.from(assetId),
+            Owner: Buffer.from(faker.name.firstName()),
+        }
+
+        console.log(`--> Submit Transaction: TransferPrivateData ${assetId} to new owner ${newOwner}`);
+        await contract.createTransaction('TransferPrivateData')
+                .setTransient(asset)
+                .submit();        
+
+        console.log('--> Submit Transaction: ReadPrivateData, function returns an asset with a given assetID');
+        result = await contract.submitTransaction('ReadPrivateData', assetId);
+    
+        res.send({status: 'OK', afterTransferPrivateDataAssetValues: StringHelper.toJSONObject(result)})
+    } catch (error) {
+        console.error(`updatePrivateData failed to run: ${error.message}`);
+        console.error( error.stack );
+        res.send({message: 'updatePrivateData failed to run'})
+    } finally {
+        console.log('Disconnect from Fabric gateway.');    
+        if (gateway != null) { gateway.disconnect(); }
+    }
+
+}
+
+
+//
 // GET /api/asset/initLedger
 //
 exports.initLedger = async (req, res) => {
--- app/routes/asset.routes.js
+++ app/routes/asset.routes.js
@@ -5,12 +5,20 @@ module.exports = app => {
     var router = require("express").Router();
 
     router.get("/enroll", assetCtrl.enroll);
+
     router.get("/initLedger", assetCtrl.initLedger)
     router.get("/getAllAssets", assetCtrl.getAllAssets)
     router.get("/createAsset", assetCtrl.createAsset)
     router.get("/readAsset", assetCtrl.readAsset)
     router.get("/updateAsset", assetCtrl.updateAsset)
     router.get("/transferAsset", assetCtrl.transferAsset)
+
+    router.get("/initPrivateData", assetCtrl.initPrivateData)
+    router.get("/getAllPrivateData", assetCtrl.getAllPrivateData)
+    router.get("/createPrivateData", assetCtrl.createPrivateData)
+    router.get("/readPrivateData", assetCtrl.readPrivateData)
+    router.get("/updatePrivateData", assetCtrl.updatePrivateData)
+    router.get("/transferPrivateData", assetCtrl.transferPrivateData)
 
     app.use('/api/asset', router);
 
